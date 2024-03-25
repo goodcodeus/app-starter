1e2a67896947dd5730d2add0d13d0bfca2b96b0e
SerhiiTsybulskyi <serhii@goodcode.us>
2024-03-25 16:23:49 +0200

    configure tailwind and update reablocks

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable no-undef */
-// eslint-disable-next-line no-undef
 module.exports = {
   plugins: [
-    require('postcss-nested'),
-    require('postcss-preset-env')({ stage: 1 }),
-    require('autoprefixer')
+    require('autoprefixer'),
+    require('tailwindcss/nesting')('postcss-nesting'),
+    require('tailwindcss'),
+    require('postcss-preset-env')({ stage: 1, 'nesting-rules': false }),
   ]
 };
