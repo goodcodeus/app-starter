1fab2fb546f857c5caf6779dc1e5886115c70276
Serhii Tsibulskiy <c4r0n0s@Serhiis-MacBook-Pro.local>
2025-10-14 14:12:02 +0300

    feat: upgrade to React 19; remove react-helmet-async; adjust bootstrap

--- src/App/NotFound/NotFound.tsx
+++ src/App/NotFound/NotFound.tsx
@@ -1,11 +1,7 @@
 import { FC } from 'react';
-import { Helmet } from 'react-helmet-async';
 
 export const NotFound: FC = () => (
   <>
-    <Helmet>
-      <title>Not Found</title>
-    </Helmet>
     <div className="w-screen h-screen flex justify-center items-center flex-col">
       <h1>404</h1>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import { ThemeProvider } from 'reablocks';
 
@@ -12,20 +11,17 @@ import { App } from './App';
 import './index.css';
 import { QueryProvider } from './core/api/QueryProvider';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryProvider>
-        <HelmetProvider>
-          <Helmet titleTemplate="%s | Starter" defaultTitle="Starter" />
-          <ThemeProvider theme={theme}>
-            <ErrorBoundary>
-              <Auth>
-                <App />
-              </Auth>
-            </ErrorBoundary>
-          </ThemeProvider>
-        </HelmetProvider>
+        <ThemeProvider theme={theme}>
+          <ErrorBoundary>
+            <Auth>
+              <App />
+            </Auth>
+          </ErrorBoundary>
+        </ThemeProvider>
       </QueryProvider>
     </BrowserRouter>
   </React.StrictMode>
